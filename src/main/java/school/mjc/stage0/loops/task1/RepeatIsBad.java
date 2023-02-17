4aa514de04b311c068df2ac6152b54d3eab5b3c6
ulvertrom <ulvertrom@gmail.com>
2023-02-17 20:20:29 +0400

    prog added

--- src/main/java/school/mjc/stage0/loops/task1/RepeatIsBad.java
+++ src/main/java/school/mjc/stage0/loops/task1/RepeatIsBad.java
@@ -4,6 +4,26 @@ public class RepeatIsBad {
 
     @SuppressWarnings("java:S106")
     public void repeatIsBad(){
-
+        String phrase = "writing the same code doesn't have much impact, and it's also time consuming";
+        System.out.println(phrase);
+        System.out.println(phrase);
+        System.out.println(phrase);
+        System.out.println(phrase);
+        System.out.println(phrase);
+        System.out.println(phrase);
+        System.out.println(phrase);
+        System.out.println(phrase);
+        System.out.println(phrase);
+        System.out.println(phrase);
+        System.out.println(phrase);
+        System.out.println(phrase);
+        System.out.println(phrase);
+        System.out.println(phrase);
+        System.out.println(phrase);
+        System.out.println(phrase);
+        System.out.println(phrase);
+        System.out.println(phrase);
+        System.out.println(phrase);
+        System.out.println(phrase);
     }
 }
